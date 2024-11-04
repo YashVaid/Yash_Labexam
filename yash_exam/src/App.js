ed5045cfc2c16e87c7b6ad34e88dc80879b18b1a
YashVaid <yashvaid460@gmail.com>
2024-11-04 16:30:51 +0530

    committtt

--- yash_exam/src/App.js
+++ yash_exam/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import  {BrowserRouter, Routes, Route} from 'react-router-dom';
 import home from './pages/home.js';
 import login from './pages/login.js';
 import Signup from './pages/Signup.js';
@@ -8,7 +9,14 @@ import Router from 'express/lib/router/index.js';
 function App() {
   return (
     <>
-    Hello
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+    
     </>
   );
 }
