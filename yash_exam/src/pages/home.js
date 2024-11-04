90f49b7af3e8d31a0b0d158a5d73d82a685b1000
YashVaid <yashvaid460@gmail.com>
2024-11-04 16:12:21 +0530

    commit

---
+++ yash_exam/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h2> Welcome to the Home Page </h2>
+      <p>This is  protected page accessible after login.</p>
+    </>
+  );
+}
+
+export default Home;
